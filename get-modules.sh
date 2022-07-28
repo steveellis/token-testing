6a4133c200d3e83dcf5331de0a599b29ff9c9adb
Steve Ellis <SEllis@corp.epnet.com>
2022-07-28 13:06:40 -0400

    Finally seeing /login-with-expiry work

---
+++ get-modules.sh
@@ -0,0 +1,6 @@
+# Using 'modules' in the path returns the modules list not the dds.
+# Using 'discovery' in the path returns the deployment descriptors.
+curl -w '\n' -X GET -D - \
+  -H "Content-type: application/json" \
+  -H "X-Okapi-Token: $TOKEN" \
+  https://core-platform-okapi.ci.folio.org/_/proxy/tenants/diku/modules
---
+++ login.sh
@@ -0,0 +1,1 @@
+curl -d '{"username":"diku_admin", "password":"admin"}' -H "Content-Type: application/json" -H "X-Okapi-Tenant: diku" -X POST https://core-platform-okapi.ci.folio.org/bl-users/login
